b47c0b3737c64fe7d04d456c1b0dd7de01b6c9e4
Saeyoung Park <spark363@my.bcit.ca>
2023-11-26 23:07:37 -0800

    Add sorting function to main page (WIP)

--- scripts/sorting.js
+++ scripts/sorting.js
@@ -1,13 +1,20 @@
-// by distance
-
-// by rating
-
-// by pet-friendly
-
 let fountains = db.collection("vancouver_drinking_fountains")
+let reviews = db.collection("reviews")
 
 console.log("sorting called")
 
-const by_pet_friendly = fountains.filter(
+const by_distance = fountains.filter(
 
 )
+console.log(by_distance);
+
+const by_rating = reviews.sort(
+    (a, b) => b.rating - a.rating
+)
+console.log(by_rating);
+
+const by_pet_friendly = fountains.filter(
+    water => water.pet_friendly == 'Y'
+
+)
+console.log(by_pet_friendly);
