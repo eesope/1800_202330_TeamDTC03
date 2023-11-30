dd422f14da769e7bbe8265a1cdd64875eabed2ef
Ella Song <msong49@my.bcit.ca>
2023-11-30 11:10:22 -0800

    DELETE the code not used anymore

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,3 @@
-db.collection('reviews').get().then((snapshot) => {
-    let first = true;
-    snapshot.forEach((doc) => {
-        if (first) {
-            var template = `<div class="water_fountains pb-2">
-                <div class="container-fluid mb-3 pb-3">
-                    <div class="title bg-white mb-3">${doc.data().name}</div>
-                    <div class="card-body bg-white">
-                        <p class="description">${doc.data().description}</p>
-                    </div>
-                    <span class="date">${new Date(doc.data().date.toDate()).toLocaleDateString()} ${new Date(doc.data().date.toDate()).toLocaleTimeString()}</span>
-                </div>
-            </div>`;
-            $('.reviewing').append(template);
-            first = false;
-        }
-    })
-})
-
 function displayWaterInfo() {
     let params = new URL(window.location.href); //get url of search bar
     let ID = params.searchParams.get("docID"); //get value for key "docID"
